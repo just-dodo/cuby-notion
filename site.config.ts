72097729fcef42f14af2be82508f8cc664631400
Dohyeon Park <dodo@acon3d.com>
2024-02-14 03:39:34 +0900

    Set pageUrlOverrides

--- site.config.ts
+++ site.config.ts
@@ -49,11 +49,9 @@ const baseConfig: SiteConfig = {
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+    '/kor': '9e4ca38bc1a24c49b213aeefd7167bf6'
+  },
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
